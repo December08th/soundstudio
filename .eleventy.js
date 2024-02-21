65dc92e52106ae142d1c943a443b8ecb55c62bb4
chalatz <chralatz@gmail.com>
2024-02-21 11:46:28 +0200

    added liquid

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const markdownIt = require("markdown-it");
+const markdownItRenderer = new markdownIt();
+
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addFilter('markdownify', (str) => {
+        return markdownItRenderer.renderInline(str);
+    });
 
     eleventyConfig.addPassthroughCopy('./assets/css/*.css');
     eleventyConfig.addPassthroughCopy('./assets/js/**/*.js');
--- index.md
+++ index.md
@@ -1,6 +1,7 @@
 ---
-layout: 'layouts/default.njk'
+layout: 'layouts/default.liquid'
 title: Home page
+info: the info
 ---
 ### Hello!
 
