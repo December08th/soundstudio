8dd9a448459b13017b5c70ff82f7c36bf996c9d8
chalatz <chralatz@gmail.com>
2024-04-18 20:39:16 +0300

    Added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./assets/js/**/*.js');
     eleventyConfig.addPassthroughCopy('./assets/images/**/*');
     eleventyConfig.addPassthroughCopy('./assets/fonts/**/*');
-
+    eleventyConfig.addPassthroughCopy('./*.png');
+    eleventyConfig.addPassthroughCopy('./favicon.ico');
+    eleventyConfig.addPassthroughCopy('.site.webmanifest');
+    
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
     return {
